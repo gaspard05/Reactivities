9559e2d8662fef2b54a89e468bbb07987c92cb87
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2020-04-01 00:11:37 +0100

    Added Mobx features; end of section 7

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,15 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useContext } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity | null;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 
-const ActivityForm: React.FC<IProps> = ({
-  setEditMode,
-  activity: initialFormState,
-  createActivity,
-  editActivity,
-  submitting,
-}) => {
+const ActivityForm: React.FC<IProps> = ({ activity: initialFormState }) => {
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -50,6 +42,8 @@ const ActivityForm: React.FC<IProps> = ({
       editActivity(activity);
     }
   };
+  const activityStore = useContext(ActivityStore);
+  const { createActivity, editActivity, submitting, cancelFormOpen } = activityStore;
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit}>
@@ -92,9 +86,9 @@ const ActivityForm: React.FC<IProps> = ({
           value={activity.venue}
         />
         <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
-        <Button onClick={() => setEditMode(false)} floated="right" type="button" content="Cancel	" />
+        <Button onClick={cancelFormOpen} floated="right" type="button" content="Cancel	" />
       </Form>
     </Segment>
   );
 };
-export default ActivityForm;
+export default observer(ActivityForm);
